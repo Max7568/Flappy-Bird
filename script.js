8e1c7b473881449099bb1215a5d9d74f295b9df9
Max7568 <maks.serwin@o2.pl>
2024-04-29 18:15:57 +0200

    up

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ function randomHeight() {
     bird.style.top = (birdTop + 3) + "px";
     };
  }, 10);
+
+ function jump() {
+    jumping = 1;
+
+    let jumpCount = 0;
+
+    let jumpInterval = setInterval(function () {
+      const birdTop =
+        parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+      if (birdTop > 6) {
+        bird.style.top = (birdTop - 6) + "px";
+      }
+      if (jumpCount > 10) {
+        clearInterval(jumpInterval);
+
+        jumping = 0;
+
+        jumpCount = 0;
+
+      }
+      jumpCount++;
+    },10);
+
+  }
